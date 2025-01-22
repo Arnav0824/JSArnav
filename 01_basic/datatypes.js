97a1634d9817b0dc4b467cbbe41e3eafacbe11f6
Arnav0824 <gautampranshoo@gmail.com>
2025-01-22 12:01:59 +0000

    conversion of datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,21 @@
+// let a=true
+// console.log(typeof a)
+// let b=Number(a)
+// console.log(b)
+//"33"-> number
+//"33bac"-> number
+//if we convert it to number then it will be NaN
+//null-> convert it-> 0
+//true->convert it->1
+// let c="hjkl"
+// console.log(typeof c)
+// let d=Boolean(c)
+// console.log(typeof d)
+// console.log(d)
+//1->true
+//"hj"->true
+//""->false
+let a=33
+let string=String(a)
+console.log(typeof string)
+console.log(string+3)
---
+++ 01_basic/variables.js
@@ -0,0 +1,12 @@
+const acId=1234
+let acMail="arnv@123"
+var acPswrd="gshjck"
+acCity="Bly"
+console.table([acId,acMail,acPswrd,acCity])
+console.log(acId)
+console.log(acMail)
+{
+    let city="hj"
+}
+let city="jk"
+console.log(city)
